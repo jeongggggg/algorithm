8ccb4f376c4c4c6c546f61a8092f9378abc7700a
User <User@jeong>
2024-06-28 00:07:45 +0900

    10998, 1008, 10869 update

--- baekjoon/src/baekjoon/step_by_step/step_01_04.java
+++ baekjoon/src/baekjoon/step_by_step/step_01_04.java
@@ -1,10 +1,41 @@
 package baekjoon.step_by_step;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+//import java.util.Scanner;
+
+/*
+	10998. A×B
+	두 정수 A와 B를 입력받은 다음, A×B를 출력하는 프로그램을 작성하시오.
+*/ 
+
 
 public class step_01_04 {
+	
+	//BufferedReader 사용
+	public static void main(String[] args) throws IOException {
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+        System.out.println(A * B);
 	}
-
+	
+	//Scanner 사용
+	
+//	 public static void main(String[] args) {
+//	        Scanner scanner = new Scanner(System.in);
+//
+//	        int a, b;
+//	        a = scanner.nextInt();
+//	        b = scanner.nextInt();
+//
+//	        int result = a * b;
+//
+//	        System.out.println(result);
+//	    }
 }
--- baekjoon/src/baekjoon/step_by_step/step_01_05.java
+++ baekjoon/src/baekjoon/step_by_step/step_01_05.java
@@ -1,10 +1,42 @@
 package baekjoon.step_by_step;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+//import java.util.Scanner;
+
+/*
+	1008. A/B
+	두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
+*/ 
+
 
 public class step_01_05 {
+	
+	// BufferedReader 사용
+	 public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+        String[] input = br.readLine().split(" ");
+        double A = Double.parseDouble(input[0]);
+		double B = Double.parseDouble(input[1]);
 
-	}
+        System.out.print(A/B);
+
+        br.close();
+    }
+	 
+	// Scanner 사용
+	 
+//	 public static void main(String[] args) {
+//		 
+//			Scanner in=new Scanner(System.in);
+//			
+//			double a=in.nextDouble();
+//			double b=in.nextDouble();
+//			
+//			in.close();
+//			System.out.print(a/b);
+//	 
+//		}
 
 }

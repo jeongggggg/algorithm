0dce73fc3bc4cb125e096092580ce4d9c7786caa
dejeong <ahjeong921@gmail.com>
2024-07-06 01:47:51 +0900

    2439 update

--- baekjoon/src/baekjoon/step_by_step/step_03_10.java
+++ baekjoon/src/baekjoon/step_by_step/step_03_10.java
@@ -1,4 +1,10 @@
 package baekjoon.step_by_step;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
 
 /*
 	2439. º°Âï±â-2
@@ -11,8 +17,62 @@ package baekjoon.step_by_step;
 public class step_03_10 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		 
+		int N = in.nextInt();
+		in.close();
+ 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k < i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
 
 	}
 
+}
+
+class step_03_10_01 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+ 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
+
+class step_03_10_02 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		int N = Integer.parseInt(br.readLine());
+		br.close();
+ 
+		for (int i = 1; i <= N; i++) {
+			for (int j = 1; j <= N - i; j++) {
+				bw.write(' ');
+			}
+			for (int k = 1; k <= i; k++) {
+				bw.write('*');
+			}
+			bw.newLine();
+		}
+		bw.flush();
+		bw.close();
+	}
 }

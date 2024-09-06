5e4915dd49f7fd4480ed36af2121c3d5e2c07f8f
dejeong <ahjeong921@gmail.com>
2024-09-06 21:45:58 +0900

    조건 문자열

---
+++ baekjoon/src/programmers/_181934.java
@@ -0,0 +1,37 @@
+package programmers;
+
+// 조건 문자열
+public class _181934 {
+    public static void main(String[] args){
+        int answer = 0;
+        String ineq = ">";
+        String eq = "=";
+        int n = 50;
+        int m = 20;
+        String str1 = ineq + eq; // <=
+        String str2 = ">="; // >=
+        String str3 = "<=";
+        String str4 = ">!";
+        String str5 = "<!";
+
+        if(str1.equals(str2)){
+            if(n >= m){
+                answer = 1;
+            }
+        }else if(str1.equals(str3)){
+            if(n <= m){
+                answer = 1;
+            }
+        }else if(str1.equals(str4)){
+            if(n > m){
+                answer = 1;
+            }
+        }else if(str1.equals(str5)){
+            if(n < m){
+                answer = 1;
+            }
+        }else{
+            answer = 0;
+        }
+    }
+}

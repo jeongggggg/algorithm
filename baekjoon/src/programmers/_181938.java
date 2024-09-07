6086040a195fb16e82d03d3843546372709f6488
dejeong <ahjeong921@gmail.com>
2024-09-07 10:12:20 +0900

    두 수의 연산값 비교하기

---
+++ baekjoon/src/programmers/_181938.java
@@ -0,0 +1,23 @@
+package programmers;
+
+// 두 수의 연산값 비교하기
+public class _181938 {
+    static class Solution {
+        public int solution(int a, int b) {
+            int answer = 0;
+            String str1 = Integer.toString(a);
+            String str2 = Integer.toString(b);
+
+            String ab = str1 + str2;
+
+            int abInt = Integer.parseInt(ab);
+
+            if(abInt < 2 * a * b){
+                answer = 2 * a * b;
+            }else{
+                answer = abInt;
+            }
+            return answer;
+        }
+    }
+}

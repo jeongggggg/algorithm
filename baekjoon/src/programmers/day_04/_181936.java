0a19c9eaf73f13ff955026197b6062027d787e1d
dejeong <ahjeong921@gmail.com>
2024-09-07 10:20:30 +0900

    Add new files to day_04 folder

---
+++ baekjoon/src/programmers/day_04/_181936.java
@@ -0,0 +1,12 @@
+package programmers.day_04;
+
+// 공배수
+public class _181936 {
+    static class Solution {
+        public int solution(int number, int n, int m) {
+            int answer = 0;
+
+            return answer = number % n == 0 && number % m == 0 ? 1 : 0;
+        }
+    }
+}
---
+++ baekjoon/src/programmers/day_04/_181937.java
@@ -0,0 +1,13 @@
+package programmers.day_04;
+
+// n의 배수
+public class _181937 {
+    static class Solution {
+        public int solution(int num, int n) {
+            int answer = 0;
+
+            return num % n == 0 ? 1 : 0;
+
+        }
+    }
+}

40f5cc234da18ad753f02069775872435e35077a
dejeong <ahjeong921@gmail.com>
2024-09-07 09:43:14 +0900

    문자열 출력하기

---
+++ baekjoon/src/programmers/_181952.java
@@ -0,0 +1,12 @@
+package programmers;
+import java.util.Scanner;
+
+// 문자열 출력하기
+public class _181952 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+
+        System.out.println(a);
+    }
+}

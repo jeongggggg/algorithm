1cc7f1bf4e1154207c3c34fc912f7c0d510c847a
dejeong <ahjeong921@gmail.com>
2024-09-07 10:08:53 +0900

    문자열 곱하기

---
+++ baekjoon/src/programmers/_181940.java
@@ -0,0 +1,12 @@
+package programmers;
+
+// 문자열 곱하기
+public class _181940 {
+    static class Solution {
+        public String solution(String my_string, int k) {
+            String answer = "";
+            answer = my_string.repeat(k);
+            return answer;
+        }
+    }
+}

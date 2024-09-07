f9a1de80b58090804bd41b51686eb8e8ce6da2c7
dejeong <ahjeong921@gmail.com>
2024-09-07 09:48:43 +0900

    문자열 붙여서 출력하기

---
+++ baekjoon/src/programmers/_181946.java
@@ -0,0 +1,13 @@
+package programmers;
+import java.util.Scanner;
+
+// 문자열 붙여서 출력하기
+public class _181946 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+
+        System.out.print(a + b);
+    }
+}

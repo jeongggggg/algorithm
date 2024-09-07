e324031c9dc19288e360e41c75d62c7f7091c05d
dejeong <ahjeong921@gmail.com>
2024-09-07 09:50:33 +0900

    홀짝 구분하기

---
+++ baekjoon/src/programmers/_181944.java
@@ -0,0 +1,12 @@
+package programmers;
+import java.util.Scanner;
+
+// 홀짝 구분하기
+public class _181944 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n % 2 == 0 ? n + " is even" : n + " is odd");
+    }
+}

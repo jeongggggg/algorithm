12d40efe7a83bade6d71873dca17ed5e552dcc59
dejeong <ahjeong921@gmail.com>
2024-09-07 09:57:33 +0900

    문자열 겹쳐쓰기

---
+++ baekjoon/src/programmers/_181943.java
@@ -0,0 +1,16 @@
+package programmers;
+
+// 문자열 겹쳐쓰기
+public class _181943 {
+    static class Solution {
+        public String solution(String my_string, String overwrite_string, int s) {
+            String answer = "";
+            answer = my_string.substring(0, s) + overwrite_string;
+
+            if(my_string.length() > answer.length()) {
+                answer += my_string.substring(answer.length());
+            }
+            return answer;
+        }
+    }
+}

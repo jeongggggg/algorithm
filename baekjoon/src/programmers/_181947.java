d465cf0b36ec14595a3156c4d2f5505e8a043f3c
dejeong <ahjeong921@gmail.com>
2024-09-07 09:47:52 +0900

    덧셈식 출력하기

---
+++ baekjoon/src/programmers/_181947.java
@@ -0,0 +1,15 @@
+package programmers;
+import java.util.Scanner;
+
+// 덧셈식 출력하기
+public class _181947 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+
+        System.out.println(a + " + " + b + " = " + c);
+    }
+
+}

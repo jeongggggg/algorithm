b01f325d9523dca882ad109cc3e6720e61fc2da3
dejeong <ahjeong921@gmail.com>
2024-09-07 09:44:01 +0900

    a와 b 출력하기

---
+++ baekjoon/src/programmers/_181951.java
@@ -0,0 +1,14 @@
+package programmers;
+import java.util.Scanner;
+
+// a와 b 출력하기
+public class _181951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}

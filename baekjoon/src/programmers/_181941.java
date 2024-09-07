e4f4284f683c66725b37732fc19510dcdc68faf2
dejeong <ahjeong921@gmail.com>
2024-09-07 10:08:23 +0900

    문자 리스트를 문자열로 변환하기

---
+++ baekjoon/src/programmers/_181941.java
@@ -0,0 +1,16 @@
+package programmers;
+
+// 문자 리스트를 문자열로 변환하기
+public class _181941 {
+    static class Solution {
+        public String solution(String[] arr) {
+            StringBuilder answer = new StringBuilder();
+
+            for (String str : arr) {
+                answer.append(str);  // 각 배열 요소를 차례로 추가
+            }
+
+            return answer.toString();  // 최종 문자열 반환
+        }
+    }
+}

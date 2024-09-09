939659faf0c1c592cfe6ad797d9a687d0eca5d9b
dejeong <ahjeong921@gmail.com>
2024-09-09 19:13:57 +0900

    정수 찾기

---
+++ src/programmers/day_23/_181840.java
@@ -0,0 +1,18 @@
+package programmers.day_23;
+
+// 정수 찾기
+public class _181840 {
+    static class Solution {
+        public int solution(int[] num_list, int n) {
+            int answer = 0;
+
+            for(int i = 0; i < num_list.length; i++){
+                if(num_list[i] == n){
+                    answer = 1;
+                }
+            }
+
+            return answer;
+        }
+    }
+}

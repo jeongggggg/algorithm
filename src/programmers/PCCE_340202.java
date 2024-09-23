5ecf46cb7b9180226a7c4e7b14d568a96aa0a29f
dejeong <ahjeong921@gmail.com>
2024-09-23 18:08:31 +0900

    Add : [PCCE 기출문제] 6번 / 물 부족

--- src/programmers/PCCE_340202.java
+++ src/programmers/PCCE_340202.java
@@ -1,2 +1,18 @@
-package programmers;public class PCCE_340202 {
+package programmers;
+
+// [PCCE 기출문제] 6번 / 물 부족
+public class PCCE_340202 {
+    static class Solution {
+        public int solution(int storage, int usage, int[] change) {
+            int total_usage = 0;
+            for(int i=0; i<change.length; i++){
+                usage = usage + usage * change[i] / 100;
+                total_usage += usage;
+                if(total_usage > storage){
+                    return i;
+                }
+            }
+            return -1;
+        }
+    }
 }
